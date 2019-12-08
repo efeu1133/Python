f7fe602d19b9a318d21c62be889c8cf5456604b6
efeu1133 <58657095+efeu1133@users.noreply.github.com>
2019-12-08 13:16:19 +0100

    Create email header.py

---
+++ email header.py
@@ -0,0 +1,11 @@
+u = ['amueller@gmx.de', 'carla2@hotmail.org', 'nicole.weber@gmx.de']
+
+x = ['from: mssleiter@burg-kl.de']
+
+y = ['to: ']
+
+for u in range (len(u)):
+    print(x[0])
+    if u==u:
+        u = u[1]
+        print(u)
