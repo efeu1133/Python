8aafbff9127b01f13921405c964eb1e7af7e0e0b
efeu1133 <efeu1133@gmail.com>
2019-12-14 13:06:58 +0100

    2

--- prime factorization.py
+++ prime factorization.py
@@ -1,8 +1,10 @@
 # Initialisierung
 primfaktoren = [[2, 2],[ 3, 7]]
 # Verarbeitung
+produkt = 1
 for element in primfaktoren:
     print(element[0],element[1])
     produkt = produkt * element [0] * element[1]
 # Ausgabe
 print('Primfaktoren:', primfaktoren)
+print('Zahl:', produkt)
