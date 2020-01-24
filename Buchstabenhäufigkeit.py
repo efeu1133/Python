298d74d5f6b2a7fabb6fc2eae8411f94c055bbff
efeu1133 <58657095+efeu1133@users.noreply.github.com>
2020-01-24 19:04:25 +0100

    Add files via upload

---
+++ Buchstabenhäufigkeit.py
@@ -0,0 +1,157 @@
+text = list(input("Eingabe: "))
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'A':
+        anzahl = anzahl + 1
+print('A', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'B':
+        anzahl = anzahl + 1
+print('B', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'C':
+        anzahl = anzahl + 1
+print('C', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'D':
+        anzahl = anzahl + 1
+print('D', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'E':
+        anzahl = anzahl + 1
+print('E', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'F':
+        anzahl = anzahl + 1
+print('F', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'G':
+        anzahl = anzahl + 1
+print('G', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'H':
+        anzahl = anzahl + 1
+print('H', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'I':
+        anzahl = anzahl + 1
+print('I', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'J':
+        anzahl = anzahl + 1
+print('J', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'K':
+        anzahl = anzahl + 1
+print('K', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'L':
+        anzahl = anzahl + 1
+print('L', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'M':
+        anzahl = anzahl + 1
+print('M', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'N':
+        anzahl = anzahl + 1
+print('N', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'O':
+        anzahl = anzahl + 1
+print('O', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'P':
+        anzahl = anzahl + 1
+print('P', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'Q':
+        anzahl = anzahl + 1
+print('Q', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'R':
+        anzahl = anzahl + 1
+print('R', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'S':
+        anzahl = anzahl + 1
+print('S', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'T':
+        anzahl = anzahl + 1
+print('T', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'U':
+        anzahl = anzahl + 1
+print('U', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'V':
+        anzahl = anzahl + 1
+print('V', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'W':
+        anzahl = anzahl + 1
+print('W', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'X':
+        anzahl = anzahl + 1
+print('X', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'Y':
+        anzahl = anzahl + 1
+print('Y', ':', anzahl)
+
+anzahl = 0
+for zeichen in text:
+    if zeichen == 'Z':
+        anzahl = anzahl + 1
+print('Z', ':', anzahl)
