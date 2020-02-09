ffc70688f0a2fdc1e8fd0f8637bf0a70f5d2ae9e
efeu1133 <58657095+efeu1133@users.noreply.github.com>
2020-02-09 11:54:26 +0100

    Add files via upload

---
+++ Tischtennisspiel.py
@@ -0,0 +1,34 @@
+#import
+import random
+
+eingabe = int(input("Eingabe: "))
+
+a = random.randint(0,40)
+b = random.randint(0,60)
+print(a)
+print(b)
+
+#1. Satz
+if eingabe ==1:
+    randompunkte = random.randint(0,11)
+print(randompunkte)
+if eingabe==1:
+        randompunkte2 = random.randint(0,11)
+print(randompunkte2)
+#2. Satz
+if eingabe==2:
+    randompunkte3 = random.randint(0,11)
+print(randompunkte3)
+if eingabe ==2:
+    randompunkte4 = random.randint(0,11)
+print(randompunkte4)
+#3. Satz
+if eingabe==3:
+    randompunkte5 = random.randint(0,11)
+print(randompunkte5)
+if eingabe==3:
+    randompunkte6 = random.randint(0,11)
+print(randompunkte6)
+#Ausgabe
+#print(Spielergebnis)
+#print(Gewinner)
