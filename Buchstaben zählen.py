5865694c85506d174588b174e0e2f95f1ac35047
efeu1133 <58657095+efeu1133@users.noreply.github.com>
2020-02-26 15:00:00 +0100

    Add files via upload

---
+++ Buchstaben zählen.py
@@ -0,0 +1,10 @@
+buchstaben = 'i'
+text = 'Das ist ein Test'
+
+
+def anzahl(zeichen,text):
+    for c in buchstaben:
+        if c in text:
+            x = text.count(c)
+        return x
+print(x)
