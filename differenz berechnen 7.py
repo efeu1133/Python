564b369ccbe6e6be1dc111f272ad07925c60fb1e
efeu1133 <58657095+efeu1133@users.noreply.github.com>
2020-03-11 18:50:07 +0100

    Add files via upload

---
+++ differenz berechnen 7.py
@@ -0,0 +1,18 @@
+def schaltjahr(jahr):
+    # s.o.
+
+def anzahlTageImMonat(monat, jahr):
+    # s.o.
+
+def naechstesDatum(datum):
+    # s.o.
+
+# Initialisierung
+datumStart = (15, 1, 2012)
+datumZiel = (16, 4, 2015)
+# Verarbeitung
+...
+# Ausgabe
+print("Startdatum:", datumStart)
+print("Zieldatum:", datumZiel)
+print("Anzahl der Tage:", anzahlTage)
