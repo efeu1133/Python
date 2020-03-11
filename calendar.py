c270306f315f69b78f693332956eb947bbcef6dd
efeu1133 <58657095+efeu1133@users.noreply.github.com>
2020-03-11 19:48:52 +0100

    Add files via upload

---
+++ calendar.py
@@ -0,0 +1,15 @@
+def leapyear(year):
+
+def AmountDaysPerMonth(month, year):
+
+def nextDate(date):
+
+# initialization
+starting_date = (1,15,2012)
+end_date = (4,16,2015)
+# process
+
+# print
+print("starting date:", starting_date)
+print("end date:", end_date)
+print("amount of days:", amount_days)
